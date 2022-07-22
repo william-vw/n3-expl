e53221ffeb7b5b32c2a799dd2956e6c0c6c0c896
william-vw <william.van.woensel@gmail.com>
2022-07-22 10:57:07 -0400

    generating html document

---
+++ src/main/resources/explain/swap/eye/output/popup.js
@@ -0,0 +1,31 @@
+function loaded() {
+	const links = document.getElementsByClassName('popuplink');
+	for (var link of links)
+		link.addEventListener('click', clicked);
+}
+
+function clicked(e) {
+	const link = e.target;
+	const info = link.nextSibling;
+
+	// show current info element
+	if (info.style.display != "block") {
+		info.style.display = "block";
+		link.innerHTML = "(hide details)"
+	} else {
+		info.style.display = "none";
+		link.innerHTML = "(show details)"
+	}
+
+	//	info.classList.add('show');
+
+	/* // hide other info elements
+	const links = document.getElementsByClassName('popuplink');
+	for (var link2 of links) {
+		if (link2 !== link) {
+			const info2 = link2.nextSibling;
+//			info.classList.remove('show');
+			info2.style.display = "none";
+		}
+	} */
+}
