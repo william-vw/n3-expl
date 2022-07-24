bf4c27de8553bea4b11856869a6ba1074aa3eb9a
william-vw <william.van.woensel@gmail.com>
2022-07-24 13:50:39 -0400

    work on jen3 impl

--- src/main/java/wvw/xai/Explainer.java
+++ src/main/java/wvw/xai/Explainer.java
@@ -14,8 +14,8 @@ import org.apache.jen3.util.IOUtils;
 public class Explainer {
 
 	public static void main(String[] args) throws Exception {
-		test();
-//		explain();
+//		test();
+		explain();
 	}
 
 	protected static void test() throws Exception {
@@ -23,7 +23,7 @@ public class Explainer {
 		spec.setFeedback(new N3Feedback(N3MistakeTypes.BUILTIN_WRONG_INPUT, FeedbackTypes.WARN, FeedbackActions.LOG));
 
 		N3Model model = ModelFactory.createN3Model(N3ModelSpec.get(Types.N3_MEM_FP_INF));
-		model.read(IOUtils.getResourceInputStream(Explainer.class, "explain/swap/jen3/test.n3"), "N3");
+		model.read(IOUtils.getResourceInputStream(Explainer.class, "cases/diabetes/case1/test_jen3.n3"), "N3");
 
 		model.getDeductionsModel().write(System.out);
 	}
@@ -33,28 +33,32 @@ public class Explainer {
 		String system = "jen3";
 		String outputType = "html";
 
-		String proofFile = "patient_red1_proof.ttl";
+		String proofFile = "patient_red1.ttl";
 
 		N3ModelSpec spec = N3ModelSpec.get(Types.N3_MEM_FP_INF);
 		spec.setFeedback(new N3Feedback(N3MistakeTypes.BUILTIN_WRONG_INPUT, FeedbackTypes.WARN, FeedbackActions.LOG));
+		spec.setFeedback(new N3Feedback(N3MistakeTypes.BUILTIN_WRONG_INPUT, FeedbackTypes.NONE, null));
 
-		N3Model model = ModelFactory.createN3Model(N3ModelSpec.get(Types.N3_MEM_FP_INF));
+		N3Model model = ModelFactory.createN3Model(spec);
 
 		model.read(IOUtils.getResourceInputStream(Explainer.class, "proofs/" + proofType + "/" + proofFile), "N3");
 
 		String explainFolder = "explain/" + proofType + "/" + system + "/";
 		model.read(IOUtils.getResourceInputStream(Explainer.class, explainFolder + "describe.n3"), "N3");
-		model.listStatements(null, model.createResource("http://wvw.org/xai#description"), (Resource) null)
+
+//		model.listStatements(null, model.createResource("http://wvw.org/xai#description"), (Resource) null)
+//				.forEachRemaining(stmt -> {
+//					System.out.println(stmt.getSubject() + " - " + stmt.getObject());
+//				});
+//		System.out.println();
+//
+		model.read(IOUtils.getResourceInputStream(Explainer.class, explainFolder + outputType + ".n3"), "N3");
+		model.listStatements(null, model.createResource("http://wvw.org/xai#output"), (Resource) null)
 				.forEachRemaining(stmt -> {
-					System.out.println(stmt.getSubject() + " - " + stmt.getObject());
+					System.out.println(stmt.getSubject() + " ? " + stmt.getObject());
 				});
 		System.out.println();
-
-		model.read(IOUtils.getResourceInputStream(Explainer.class, explainFolder + outputType + ".n3"), "N3");
-//		model.listStatements(null, model.createResource("http://wvw.org/xai#output"), (Resource) null)
-//				.forEachRemaining(stmt -> {
-//					System.out.println(stmt.getSubject() + " ? " + stmt.getObject());
-//				});
+		
 		model.listStatements(model.createResource("http://wvw.org/xai"),
 				model.createResource("http://wvw.org/xai#output"), (Resource) null).forEachRemaining(stmt -> {
 					System.out.println("output: " + stmt.getObject());
