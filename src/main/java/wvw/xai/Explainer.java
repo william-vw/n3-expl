aa03617baf51228d782bea1f13e6eced20f18b8c
william-vw <william.van.woensel@gmail.com>
2022-07-31 11:01:55 -0400

    work on getting jen3 to work

--- src/main/java/wvw/xai/Explainer.java
+++ src/main/java/wvw/xai/Explainer.java
@@ -1,5 +1,7 @@
 package wvw.xai;
 
+import java.io.File;
+
 import org.apache.jen3.n3.FeedbackActions;
 import org.apache.jen3.n3.FeedbackTypes;
 import org.apache.jen3.n3.N3Feedback;
@@ -28,23 +30,30 @@ public class Explainer {
 		model.getDeductionsModel().write(System.out);
 	}
 
+	// replace-all: @forAll (var:[^\{]*) -> ""
 	protected static void explain() throws Exception {
 		String proofType = "swap";
 		String system = "jen3";
-		String outputType = "html";
 
-		String proofFile = "patient_red1.ttl";
+//		String proofFile = "patient_red1.ttl";
+//		String labelFile = null;
+
+		String proofFile = "diabetes_case2.ttl";
+		String labelFile = "cases/diabetes/labels.ttl";
 
 		N3ModelSpec spec = N3ModelSpec.get(Types.N3_MEM_FP_INF);
-		spec.setFeedback(new N3Feedback(N3MistakeTypes.BUILTIN_WRONG_INPUT, FeedbackTypes.WARN, FeedbackActions.LOG));
-		spec.setFeedback(new N3Feedback(N3MistakeTypes.BUILTIN_WRONG_INPUT, FeedbackTypes.NONE, null));
+		spec.setFeedback(new N3Feedback(N3MistakeTypes.BUILTIN_WRONG_INPUT, FeedbackTypes.NONE, FeedbackActions.LOG));
 
 		N3Model model = ModelFactory.createN3Model(spec);
+
+		if (labelFile != null)
+			model.read(IOUtils.getResourceInputStream(Explainer.class, labelFile), "N3");
 
 		model.read(IOUtils.getResourceInputStream(Explainer.class, "proofs/" + proofType + "/" + proofFile), "N3");
 
 		String explainFolder = "explain/" + proofType + "/" + system + "/";
 		model.read(IOUtils.getResourceInputStream(Explainer.class, explainFolder + "describe.n3"), "N3");
+		model.read(IOUtils.getResourceInputStream(Explainer.class, explainFolder + "collect.n3"), "N3");
 
 //		model.listStatements(null, model.createResource("http://wvw.org/xai#description"), (Resource) null)
 //				.forEachRemaining(stmt -> {
@@ -52,19 +61,30 @@ public class Explainer {
 //				});
 //		System.out.println();
 //
-		model.read(IOUtils.getResourceInputStream(Explainer.class, explainFolder + outputType + ".n3"), "N3");
-		model.listStatements(null, model.createResource("http://wvw.org/xai#output"), (Resource) null)
-				.forEachRemaining(stmt -> {
-					System.out.println(stmt.getSubject() + " ? " + stmt.getObject());
-				});
-		System.out.println();
-		
-		model.listStatements(model.createResource("http://wvw.org/xai"),
-				model.createResource("http://wvw.org/xai#output"), (Resource) null).forEachRemaining(stmt -> {
-					System.out.println("output: " + stmt.getObject());
-				});
+//		model.listStatements(null, model.createResource("http://wvw.org/xai#conclusionStr"), (Resource) null)
+//				.forEachRemaining(stmt -> {
+//					System.out.println(stmt.getSubject() + " => " + stmt.getObject());
+//				});
+//		System.out.println();
+//
+//		model.listStatements(null, model.createResource("http://wvw.org/xai#output"), (Resource) null)
+//				.forEachRemaining(stmt -> {
+//					System.out.println(stmt.getSubject() + " ? " + stmt.getObject());
+//				});
+//		System.out.println();
 
+		String html = model.outputString();
+//		System.out.println(html);
+		
+		String resDir = System.getProperty("user.dir") + "/src/main/resources/";
+		String htmlDir = resDir + "output/jen3/";
+		
+		String fileName = proofFile.substring(0, proofFile.indexOf(".ttl"));
+		File htmlFile = new File(htmlDir + fileName + ".html");
+		IOUtils.writeToFile(html, htmlFile);
+		System.out.println("written to " + htmlFile.getAbsolutePath());
+		
 //		model.write(System.out);
-//		model.getDeductionsModel().write(System.out);
+		model.getDeductionsModel().write(System.out);
 	}
 }
