a6bf0be3e7c11472f457d0b3fe24876863493c93
William Van Woensel <wvw@Williams-MacBook-Pro.local>
2022-11-15 11:16:15 -0500

    2 explain 'modes'

--- src/main/resources/explain.sh
+++ src/main/resources/explain.sh
@@ -3,7 +3,7 @@
 #!/bin/bash
 usage()
 {
-	echo -e "usage: explain.sh [-r|--rules]* [-d|--data] ([-l|--labels]) [-q|--query_proof] [-i|--inf_out] [-p|--pe_out] [-h|--html_out]\n"
+	echo -e "usage: explain.sh [-d|--data] [-r|--rules]* ([-l|--labels]) [-q|--query_proof] [-x|--xexplain] [-i|--inf_out] [-p|--pe_out] [-h|--html_out]\n"
 }
 
 if [ "$1" == "" ]; then
@@ -11,15 +11,19 @@ if [ "$1" == "" ]; then
 	exit 1
 fi
 
-aux_all=explain/swap/eye/aux_all.n3
-aux_query=explain/swap/eye/aux_query.n3
+aux_query_top=explain/swap/eye/aux_query_top.n3
+aux_query_all=explain/swap/eye/aux_query_all.n3
+query_heads=explain/swap/eye/query_heads.n3
+query_xexplain=explain/swap/eye/query_xexplain.n3
 describe=explain/swap/eye/describe.n3
 collect=explain/swap/eye/collect.n3
 query_html=explain/swap/eye/query.n3
 
+xexplain=0
 inf_out=inf_out.ttl
 pe_out=pe_out.ttl
 html_out=out.html
+target_out=target_out.ttl
 labels=
 
 while [ "$1" != "" ]; do
@@ -35,6 +39,8 @@ while [ "$1" != "" ]; do
 								;;
 	-q | --query_proof ) 		shift
 								query_proof=$1
+								;;
+	-x | --xexplain )			xexplain=1
 								;;
 	-i | --inf_out )			shift
 								inf_out=$1
@@ -66,34 +72,35 @@ $(eye --pass-only-new --nope --n3 $data $rules > $inf_out)
 # - print proof
 
 if [ ! -z "$data" ]; then
-	rules="$data\n$rules"
+	rules="$data $rules"
 fi
 
-# (1) print entire deductive closure. 
+# - print entire deductive closure. 
 # (the proof file will include a lot of redundant lemmas)
+#aux=$aux_query
 #$(eye --pass --n3 $rules > $pe_out)
 
-# (2) use query
-
-# (2.1) if proof_query is not given: only print inferences by our rules
-#if [ -z "$query_proof" ]; then
-#	aux=$aux_query_all
-#	$(eye --pass --n3 $rules > $pe_out)
-#else
-#	aux=$aux_query_target
-#	$(eye --n3 $rules --query $query_proof > $pe_out)
-#fi
-
-# (2.2) else: only print inferences matching the query_proof
-# (has duplicates; also, lemmas for query will list other lemmas multiple times)
-if [ -z "$query_proof" ]; then	
-	query_proof=$rules
-	aux=$aux_all
+# - use query
+if [ -z "$query_proof" ]; then
+	# -- prove x:Explain triples
+	if [ $xexplain -eq 1 ]; then
+		# query for x:Explain properties
+		$(eye --nope --n3 $rules --query $query_xexplain > $target_out)
+		# (print all inferences)
+		aux=$aux_query_all
+	# -- prove rule head triples
+	else
+		# query for all rule heads
+		$(eye --nope --n3 $rules --query $query_heads > $target_out)
+		# (print top-level inferences)
+		aux=$aux_query_top
+	fi	
+	# prove either x:Explain triples or rule head triples	
+	query_proof=$target_out
 else
-	aux=$aux_query
+	aux=$aux_query_all
 fi
 $(eye --n3 $rules --query $query_proof > $pe_out)
-
 
 
 # - print html
